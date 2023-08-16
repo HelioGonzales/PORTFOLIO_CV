9897a8c01f98901323db3c834ba42e7c2e940ece
HelioGonzales <heliogonzalesr@gmail.com>
2023-08-16 22:59:12 +0200

    feat: contact and footer section

--- script.js
+++ script.js
@@ -18,4 +18,34 @@
     $menu.classList.remove('is-active');
 
   })
-})(document)
+})(document);
+
+((d) => {
+  const $form = d.querySelector('.contact-form'),
+    $loader = d.querySelector('.contact-form-loader'),
+    $response = d.querySelector('.contact-form-response');
+
+  $form.addEventListener("submit", e => {
+    e.preventDefault();
+    $loader.classList.remove('none');
+    fetch('https://formsubmit.co/ajax/heliogonzalesr@gmail.com', {
+      method: 'POST',
+      body: new FormData(e.target)
+    })
+      .then(res => res.ok ? res.json() : Promise.reject(res))
+      .then(json => {
+        location.hash = '#thankyou';
+        $form.reset();
+      })
+      .catch(err => {
+        console.log(err);
+        let message = err.statusText || `An error ocurred, please try again`
+        $response.querySelector('h3').innerHTML = `Error ${err}: ${message}`;
+      }).finally(() => {
+        $loader.classList.add('none');
+        setTimeout(() => {
+          location.hash = '#close';
+        }, 3000);
+      });
+  })
+})(document);
